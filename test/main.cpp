8c71e9d37b4f79f8323a3f10313d3ef9f61fb79d
Krzysiek Karbowiak <17130511+kkarbowiak@users.noreply.github.com>
2017-12-06 23:25:00 +0100

    Add simple test

---
+++ test/main.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <string>
+#include <list>
+#include <iostream>
+
+#include "iterWalk.h"
+
+
+template<class C>
+void do_walk(C & c)
+{
+    for (auto i : iter::walk(c))
+    {
+        std::cout << "first: " << i.is_first() << ", last: " << i.is_last() << ", index: " << i.get_index() << ", value: " << *i << '\n';
+    }
+}
+
+int main()
+{
+    std::vector<int> ivec = {3, 5, 7, 8, 9};
+    std::vector<float> fvec = {.3, .5, .7, .8, .9};
+    std::vector<std::string> svec = {"one", "two", "three", "four", "five"};
+    std::list<int> ilis = {8, 7, 6, 5, 4};
+
+    do_walk(ivec);
+    do_walk(fvec);
+    do_walk(svec);
+    do_walk(ilis);
+}
